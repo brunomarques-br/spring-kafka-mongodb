285b954d0632c438fa7b126c5b0db3941ada59ce
Bruno Marques <brunomcamilo.br@gmail.com>
2025-03-23 01:10:29 -0300

    order-service: Criando endpoints REST para order (POST) e event (GET)

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -3,5 +3,18 @@ package br.com.microservices.orchestrated.orderservice.core.repository;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EventRepository extends MongoRepository<Event, String> {
+
+    // This method is used to find all events by order by created at desc.
+    List<Event> findAllByOrderByCreatedAtDesc();
+
+    // This method is used to find a last event by order id.
+    Optional<Event> findTop1ByOrderByCreatedAtDesc(String orderId);
+
+    // This method is used to find a last event by transaction id.
+    Optional<Event> findTop1ByTransactionIdOrderByCreatedAtDesc(String transactionId);
+
 }
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,12 +1,18 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
+import br.com.microservices.orchestrated.orderservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.dto.EventFilters;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Slf4j
 @Service
@@ -21,6 +27,37 @@ public class EventService {
         event.setCreatedAt(LocalDateTime.now());
         save(event);
         log.info("Order: {} with saga notified! TransactionId: {}", event.getOrderId(), event.getTransactionId());
+    }
+
+    public List<Event> findAll(){
+        return repository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public Event findByFilters(EventFilters filters) {
+        validateEmptyFilters(filters);
+        if(!isEmpty(filters.getOrderId())){
+            return findByOrderId(filters.getOrderId());
+        }else {
+            return findByTransactionId(filters.getTransactionId());
+        }
+    }
+
+    private Event findByOrderId(String orderId) {
+        return repository.findTop1ByOrderByCreatedAtDesc(orderId).orElseThrow(
+                () -> new ValidationException("Order not found by orderId")
+        );
+    }
+
+    private Event findByTransactionId(String transactionId) {
+        return repository.findTop1ByTransactionIdOrderByCreatedAtDesc(transactionId).orElseThrow(
+                () -> new ValidationException("Order not found by TransactionId")
+        );
+    }
+
+    private void validateEmptyFilters(EventFilters filters) {
+        if (isEmpty(filters.getOrderId()) && isEmpty(filters.getTransactionId())) {
+            throw new ValidationException("OrderId and transactionId must be informed.");
+        }
     }
 
     public Event save(Event event) {
