1c6771af6455eb803a1d0d094c0722a29acd554e
Bruno Marques <brunomcamilo.br@gmail.com>
2025-03-24 17:12:57 -0300

    inventory-service: criando a carga iniciado do inventory-service

---
+++ inventory-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.inventory(id, product_code, avaliable) VALUES (1, 'COMIC_BOOKS', 4);
+INSERT INTO public.inventory(id, product_code, avaliable) VALUES (2, 'BOOKS', 2);
+INSERT INTO public.inventory(id, product_code, avaliable) VALUES (3, 'MOVIES', 5);
+INSERT INTO public.inventory(id, product_code, avaliable) VALUES (4, 'MUSIC', 9);
