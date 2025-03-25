35dae3bcb21dfcd4cdbd8a1fbdc8f4a10ea5d599
Bruno Marques <brunomcamilo.br@gmail.com>
2025-03-25 17:12:01 -0300

    inventory-service: Criando a lógica para atualizar o inventario

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -2,7 +2,10 @@ package br.com.microservices.orchestrated.inventoryservice.core.service;
 
 import br.com.microservices.orchestrated.inventoryservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.History;
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Order;
 import br.com.microservices.orchestrated.inventoryservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus;
 import br.com.microservices.orchestrated.inventoryservice.core.model.Inventory;
 import br.com.microservices.orchestrated.inventoryservice.core.model.OrderInventory;
 import br.com.microservices.orchestrated.inventoryservice.core.producer.KafkaProducer;
@@ -12,6 +15,8 @@ import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -29,6 +34,8 @@ public class InventoryService {
         try {
             checkCurrentValidation(event);
             createOrderInventory(event);
+            updateInventory(event.getPayload());
+            handleSuccess(event);
         } catch (Exception e) {
             log.error("Error trying to update inventory", e);
         }
@@ -68,6 +75,57 @@ public class InventoryService {
     private Inventory findInventoryByProductCode(String productCode) {
         return inventoryRepository.findByProductCode(productCode)
                 .orElseThrow(() -> new ValidationException("Inventory not found for product_code: " + productCode));
+    }
+
+    /**
+     * Method to update the inventory for each product
+     * @param order
+     */
+    private void updateInventory(Order order){
+        order.getProducts().forEach(product -> {
+            var inventory = findInventoryByProductCode(product.getProduct().getCode());
+            checkInventory(inventory.getAvaliable(), product.getQuantity());
+            inventory.setAvaliable(inventory.getAvaliable() - product.getQuantity());
+            inventoryRepository.save(inventory);
+        });
+    }
+
+    /**
+     * Method to check if the inventory is available
+     * @param avaliable
+     * @param orderQuantity
+     */
+    private void checkInventory(int avaliable, int orderQuantity) {
+        if (orderQuantity > avaliable) {
+            throw new ValidationException("Product is out of stock!");
+        }
+    }
+
+    /**
+     * Method to handle the success of the payment
+     *
+     * @param event
+     */
+    private void handleSuccess(Event event) {
+        event.setStatus(ESagaStatus.SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Inventory updated successfully!");
+    }
+
+    /**
+     * Method to add a history to the event
+     *
+     * @param event
+     * @param message
+     */
+    private void addHistory(Event event, String message) {
+        var history = History.builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
     }
 
 }
